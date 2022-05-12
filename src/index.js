0ac5a7b5ea2bec7cb91344b34da5b59893094623
brandmch <101467867+brandmch@users.noreply.github.com>
2022-05-12 15:07:15 -0500

    Components created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Header from './Components/header';
+import Form from './Components/form';
+import Meme from './Components/meme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>hiiiii</h1>
+    <Header />
+    <Form />
+    <Meme />
   </React.StrictMode>
 );
